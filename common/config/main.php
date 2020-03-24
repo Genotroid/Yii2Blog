5c1f1af8cb41e3be6da6204ee31b463e4ff91359
Genotroid <genotroid@gmail.com>
2020-03-24 05:23:07 +0500

    add administrator to admin group

--- common/config/main.php
+++ common/config/main.php
@@ -15,6 +15,7 @@ return [
 
         'user' => [
             'class' => 'dektrium\user\Module',
+            'admins' => ['administrator']
         ],
         'files' => [
             'class' => 'floor12\files\Module',
